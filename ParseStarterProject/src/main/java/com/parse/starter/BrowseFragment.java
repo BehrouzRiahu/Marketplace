45c97074a2e40d668660e29c852b763ddaba02d1
Ziyad33 <mzn.93.20@gmail.com>
2016-01-08 03:01:10 +0800

    MainActivity implemented

---
+++ ParseStarterProject/src/main/java/com/parse/starter/BrowseFragment.java
@@ -0,0 +1,9 @@
+package com.parse.starter;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mazinalmaskari on 1/8/16.
+ */
+public class BrowseFragment extends Fragment {
+}
--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -8,22 +8,55 @@
  */
 package com.parse.starter;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.parse.ParseAnalytics;
 
+import java.util.Locale;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
+
+  SectionsPagerAdapter mSectionPagerAdapter;
+
+  ViewPager mViewPager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    final ActionBar actionBar = getSupportActionBar();
+    actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blue)));
+    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+    mSectionPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+    mViewPager = (ViewPager) findViewById(R.id.pager);
+    mViewPager.setAdapter(mSectionPagerAdapter);
+
+    mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+      public void onPageSelected(int position){
+        actionBar.setSelectedNavigationItem(position);
+      }
+    });
+
+    for(int i = 0; i <mSectionPagerAdapter.getCount(); i++){
+      actionBar.addTab(actionBar.newTab().setText(mSectionPagerAdapter.getPageTitle(i)).setTabListener(this));
+      actionBar.setSplitBackgroundDrawable(getResources().getDrawable(android.R.color.white));
+      actionBar.setStackedBackgroundDrawable(getResources().getDrawable(R.color.blue));
+    }
+
   }
 
   @Override
@@ -46,5 +79,55 @@ public class MainActivity extends ActionBarActivity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+    mViewPager.setCurrentItem(tab.getPosition());
+
+  }
+
+  @Override
+  public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+  }
+
+  @Override
+  public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+  }
+
+  public class SectionsPagerAdapter extends FragmentPagerAdapter{
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+      super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+      if(position == 0){
+        return new BrowseFragment();
+      }else{
+        return new SearchFragment();
+      }
+    }
+
+    @Override
+    public int getCount() {
+      return 2;
+    }
+
+    public CharSequence getPageTitle(int position){
+      Locale l = Locale.getDefault();
+      switch (position){
+        case 0:
+          return "Browse".toUpperCase(l);
+        case 1:
+          return "Search".toUpperCase(l);
+      }
+      return null;
+
+    }
+
   }
 }
